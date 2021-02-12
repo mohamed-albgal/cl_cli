5ca49a3bbb422af0bfde234b7f6e389cbbee46bf
Mohamed <malbgal@gmail.com>
2021-02-11 21:27:28 -0800

    reformatted minor

--- cl.py
+++ cl.py
@@ -3,67 +3,85 @@ from craigslist import CraigslistForSale
 import os
 import sys
 
-def cl_search(filters, lim):
-	print(filters)
+def cl_search(filters, limit):
+	#api takes an dict for filters and their values, including search query price etc
+	# limit is needed but is not a key in the dict, rather, its used at a later step
 	try:
-		lim = int(lim)
 		cl_fs = CraigslistForSale(site='sfbay', area='sby', category='sss',filters=filters)
-		resp = cl_fs.get_results(sort_by='newest', limit=lim)
-		results = [f"open {result['url']}" for result in resp]
+		resp = cl_fs.get_results(sort_by='newest', limit=int(limit))
+		results = [result['url'] for result in resp]
 		if not results:
 			print(f"\nNothing found for {filters['query']}")
 			results = None
 		return results
-	except Exception:
+	except Exception as err:
+		print(err)
 		print(f"There was an error looking that up, please make sure field values are valid:\n\t{filters}")
 		return None
 
-def batchOpen(opencommands):
-	if opencommands is None: return
-	length = len(opencommands)
+def batchOpen(urls):
+	if urls is None: return
+	length = len(urls)
 	numberNewTabs = 5 if length >= 5 else length
-	show = input(f"Found {len(opencommands)} listings, open {numberNewTabs} in the browser?\t(y/n)")
+	show = input(f"Found {len(urls)} listings, open {numberNewTabs} in the browser?\t(y/n)")
 	if show.lower() == 'n': return
 	count = 0
 	while show.lower() != 'q':
 		for i in range(numberNewTabs):
-				if not opencommands:
+				if not urls:
 					show = 'q'
 					break;
-				os.system(opencommands.pop())
+				os.system(f"open {urls.pop()}")
 				count += 1
 		moreleft = numberNewTabs if numberNewTabs < length - count else length-count
 		if moreleft:
 			show = input(f"Press any key to show {moreleft} more, press q to quit this search \t")
 	print(f"Search Completed")
 
-def promptForSearchParams(filters={}):
-	filters['query'] = input("Search Term: ") or None
-	filters['min_price'] = input("Min price: ") or None
-	filters['max_price'] = input("Max price: ") or None
-	lim = input("Max Results To Show: ") or 300
-	filters['posted_today'] = False if input("Only today\'s posts?)(y/n)\t").lower() == 'n' else True
-	return (filters,lim)
+def promptForSearchParams(params={}):
+	params['query'] = input("Search Term: ") or None
+	params['min_price'] = input("Min price: ") or None
+	params['max_price'] = input("Max price: ") or None
+	limit = input("Max Results To Show: ") or None
+	params['posted_today'] = False if input("Only today\'s posts?)(y/n)\t").lower() == 'n' else True
+
+	return limit, params
 
 
 def main(args=[None]):
 
+	queryModifiers = ['query', 'min_price', 'max_price', 'limit']
 	while True:
 		argcount = len(args)
-		lim = 0
-		filters = {}
+		apiparams = {}
+		limit = None
+			#buggy, enclosed string still counted as many tokens instead of one
 		if argcount == 1:
-			filters,lim=promptForSearchParams(filters);
+			limit, apiparams=promptForSearchParams();
 		else:
-			fields = ['query', 'min_price', 'max_price']
-			filters = {fields[i-1]:args[i] for i in range(1,len(args))}
-			lim = 1000 if argcount <= 4 else args[4]
-			filters["posted_today"] = True
-		batchOpen(cl_search(filters,lim))
-		repeat = input("To make another search type \"y \"\t ")
-		if repeat.lower() != "y":
+			apiparams = dict([x for x in zip(queryModifiers,args[1:])])
+			apiparams["posted_today"] = True
+			if 'limit' in apiparams:
+				limit = apiparams['limit']
+				del apiparams['limit']
+		limit = 200 or limit
+		urls = cl_search(apiparams, limit)
+		batchOpen(urls)
+		if input("To make another search type \"y \"\t ").lower() != "y":
 			break
 	quit()
 
 if __name__ == "__main__":
+	stars = "*"*60
+	line = "-"*60
+	cl = '{:^60}'.format("CRAIGSLIST CLI")
+	print(stars)
+	print(cl)
+	print(stars)
+	print(line)
+
 	main(sys.argv)
+
+	print(line)
+	print(stars)
+	print(stars)
