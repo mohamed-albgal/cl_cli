98db45bc4d7bf0adbba069be2f680f7323616a8a
Mohamed <malbgal@gmail.com>
2021-04-01 16:11:14 -0700

    fixed sh script path

--- cl.sh
+++ cl.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
-#acitvate the virtualenv (so the cl module is imported
-source ~/Desktop/python/cl/cl_cli/venv/bin/activate
 
-#the first arg is the query param
+# this assumes virtualenv is named "venv"
+
+#acitvate the virtualenvj
+source ./venv/bin/activate
+
 #quotation marks around the $@ causes retention of user-input quotation marks
-python3 ~/Desktop/python/cl/cl_cli/src/cl.py "$@"
+python3 ./src/cl.py "$@"
 
 #deactivate the virutalenv after completion
 deactivate
